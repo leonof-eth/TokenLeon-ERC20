1674f2f6a7ba6c592ea25a533bb9443123c3cabc
leonof-eth <leonofeth@gmail.com>
2025-10-27 01:19:30 +0700

    Add files via upload

---
+++ TokenLeon.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TokenLeon is ERC20, Ownable {
+    
+    constructor() ERC20("TokenLeon", "TLN") Ownable(msg.sender) {
+        _mint(msg.sender, 1000 * 10 ** decimals());
+    }
+    
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount * 10 ** decimals());
+    }
+    
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount * 10 ** decimals());
+    }
+}
